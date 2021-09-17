0afb19bb5c237af2afae7b59d6c9e302a72d0510
Wallace-A <andrew.wallace.english@gmail.com>
2021-09-17 15:49:59 +0900

    fetch request for weather and date formatting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <main>
     <div class="search-box">
-      <input type="text" class="search-bar" placeholder="search..."/>
+      <!-- v-model uses data for string value -->
+      <!-- @keypress is short for v-on:keypress, any keypress will fire function -->
+      <input 
+      type="text" 
+      class="search-bar" 
+      placeholder="search..."
+      v-model="query"
+      @keypress="fetchWeather"
+      />
     </div>
-    <div class="weather-wrap">
+    <!-- v-if will only use variables (weather.main, etc) if weather.main is undefined -->
+    <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
       <div class="location-box">
-        <div class="location">Yokohama, Japan</div>
-        <div class="date">Friday 17 September 2020</div>
+        <div class="location">{{ weather.name }},{{ weather.sys.country}}</div>
+        <div class="date"> {{ dateBuilder() }} </div>
       </div>
       <div class="weather-box">
-        <div class="temp">9°C</div>
-        <div class="weather">Rain</div>
+        <div class="temp">{{ Math.round(weather.main.temp)}}°C</div>
+        <div class="weather">{{weather.weather[0].main}}</div>
       </div>
     </div>
   </main>
@@ -21,7 +30,35 @@ export default {
   name: 'App',
   data () {
     return {
-      api_key: "fb03124f0ef820f6344d90f58c6e5af2"
+      api_key: "fb03124f0ef820f6344d90f58c6e5af2",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if(e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
